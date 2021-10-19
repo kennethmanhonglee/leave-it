7351fca6e317b1c078b9c92c95cf3301d6f97f06
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-19 16:45:16 -0700

    changed font and added default styling

---
+++ react-app/src/components/Navbar/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+import LogoutButton from "../auth/LogoutButton";
+import PetForm from "../PetForm";
+import styles from "./Navbar.module.css";
+
+const NavBar = () => {
+  const currentUser = useSelector((state) => state.session.user);
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="/" exact={true} activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        {!currentUser && (
+          <>
+            <li>
+              <NavLink to="/login" exact={true} activeClassName="active">
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/sign-up" exact={true} activeClassName="active">
+                Sign Up
+              </NavLink>
+            </li>
+          </>
+        )}
+        {currentUser && (
+          <>
+            <li>
+              <NavLink to="/add_a_pet" exact>
+                Add a pet
+              </NavLink>
+            </li>
+            <li>
+              <LogoutButton />
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
