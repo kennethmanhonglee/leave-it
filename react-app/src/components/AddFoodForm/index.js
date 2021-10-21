315f8af394489a223cc0b6502022b860a615ddc6
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-21 14:16:33 -0700

    have a structure on add food form, now working on functionality of create food form

--- react-app/src/components/AddFoodForm/index.js
+++ react-app/src/components/AddFoodForm/index.js
@@ -1,8 +1,13 @@
-import { NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import styles from "./AddFoodForm.module.css";
 
 const AddFoodForm = () => {
+  const history = useHistory();
+  const createFood = () => {
+    return history.push("/create_food");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header_bar}>
@@ -13,7 +18,9 @@ const AddFoodForm = () => {
           </h3>
         </div>
         <div className={styles.add_button_div}>
-          <button className={styles.button}>Create a Food</button>
+          <button onClick={createFood} className={styles.button}>
+            Create a Food
+          </button>
         </div>
       </div>
       <div className={styles.food_entries}>
