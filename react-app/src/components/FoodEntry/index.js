0059981fbc9ee3dd4e7fa653391d0fe4d11798a2
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-22 11:05:26 -0700

    added meals slice of state

--- react-app/src/components/FoodEntry/index.js
+++ react-app/src/components/FoodEntry/index.js
@@ -1,8 +1,12 @@
 import styles from "./FoodEntry.module.css";
 
 const FoodEntry = ({ food }) => {
+  const addFood = async () => {
+    //   call thunk to create a meal
+    console.log(food.id);
+  };
   return (
-    <>
+    <div className={styles.food_entry}>
       <div className={styles.food_info}>
         <h2>{food.food_name}</h2>
         <h2>{food.food_type}</h2>
@@ -10,9 +14,12 @@ const FoodEntry = ({ food }) => {
         <h2>{food.serving_size} g</h2>
       </div>
       <div className={styles.add_button_div}>
-        <button className={styles.button}>Add</button>
+        {/* placeholder, will likely use a fontawesome icon later */}
+        <button onClick={addFood} className={styles.button}>
+          Add
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ react-app/src/store/meal.js
@@ -0,0 +1,18 @@
+// constants
+
+// actions
+
+// thunks
+
+// reducer
+const initialState = {};
+
+const reducer = (state = initialState, action) => {
+  let newState = Object.assign({}, state);
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
