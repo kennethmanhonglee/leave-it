72a7c57b83664017a9eee380d65781539bda9754
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-26 08:30:12 -0700

    added slight styling to splash page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,18 +1,20 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import styles from "./SplashPage.module.css";
 
 const SplashPage = () => {
   return (
     <div className={styles.splash_page}>
-      <div className={styles.phone_pic}>PHONE PLACEHOLDER</div>
-      <div className={styles.slogan}>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
+      <div className={styles.slogan_container}>
+        <div className={styles.slogan}>
+          <h1>Weight loss for dogs</h1>
+          <h4>Track the foods your dog loves</h4>
+        </div>
+        <Link className={styles.link} to="/sign-up" exact>
+          <div className={styles.signup_button}>Sign Up</div>
+        </Link>
       </div>
+      <div className={styles.bio}>this is bio and stuff</div>
     </div>
   );
 };
