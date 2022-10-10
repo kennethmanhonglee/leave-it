58dbb59ea99d145907ec1e8e4d51c203be3633a9
Kenneth Lee <kenneth.lee.developer@gmail.com>
2022-10-10 13:10:19 -0700

    Fix pre-commit to not yell at app/config.py

--- app/aws.py
+++ app/aws.py
@@ -1,12 +1,12 @@
-import uuid
-import boto3
-import botocore
 import os
+import uuid
+
+import boto3
 
 s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.environ.get('S3_KEY'),
-    aws_secret_access_key=os.environ.get('S3_SECRET')
+    "s3",
+    aws_access_key_id=os.environ.get("S3_KEY"),
+    aws_secret_access_key=os.environ.get("S3_SECRET"),
 )
 
 
@@ -14,8 +14,7 @@ ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
 
 
 def allowed_file(filename):
-    return "." in filename and \
-           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def get_unique_filename(filename):
@@ -34,10 +33,7 @@ def upload_file_to_s3(file, acl="public-read"):
             file,
             BUCKET_NAME,
             file.filename,
-            ExtraArgs={
-                "ACL": acl,
-                "ContentType": file.content_type
-            }
+            ExtraArgs={"ACL": acl, "ContentType": file.content_type},
         )
     except Exception as e:
         # in case the our s3 upload fails
@@ -47,17 +43,17 @@ def upload_file_to_s3(file, acl="public-read"):
 
 
 def delete_from_s3(image_url):
-    '''
+    """
     Taken from Cleanstagram. FIXME check and see if needed
-    '''
+    """
     split_url = image_url.split(S3_LOCATION)
     if len(split_url) == 2:
         filename = image_url.split(S3_LOCATION)[1]
     else:
         # filename not from s3
-        return {'ok': True}
+        return {"ok": True}
     try:
         s3.delete_object(Bucket=BUCKET_NAME, Key=filename)
-        return {'ok': True}
+        return {"ok": True}
     except Exception as e:
-        return {'errors': str(e)}
+        return {"errors": str(e)}
